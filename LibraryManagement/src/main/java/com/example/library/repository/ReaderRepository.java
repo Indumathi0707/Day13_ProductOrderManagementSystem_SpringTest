9892cb0192c694625ede6777c8793adae6705b1d
Indumathi0707 <161193458+Indumathi0707@users.noreply.github.com>
2025-08-05 09:04:01 +0530

    Delete LibraryManagement directory

--- LibraryManagement/src/main/java/com/example/library/repository/ReaderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.library.repository;
-
-import com.example.library.entity.Reader;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReaderRepository extends JpaRepository<Reader, Long> {
-}
