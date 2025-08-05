294aee86c506bbda827bf78c66338cd61b67910d
Indumathi0707 <161193458+Indumathi0707@users.noreply.github.com>
2025-08-05 09:03:30 +0530

    Delete HospitalManagement directory

--- HospitalManagement/src/main/java/com/example/hospital/repository/DoctorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.hospital.repository;
-
-import com.example.hospital.entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {}
--- HospitalManagement/src/main/java/com/example/hospital/repository/MedicalRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.hospital.repository;
-
-import com.example.hospital.entity.MedicalRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {}
