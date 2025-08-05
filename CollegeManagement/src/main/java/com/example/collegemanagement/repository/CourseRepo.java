6780a166fac67b77bc4210e3e965d4c03c2454be
Indumathi0707 <161193458+Indumathi0707@users.noreply.github.com>
2025-08-05 09:03:05 +0530

    Delete CollegeManagement directory

--- CollegeManagement/src/main/java/com/example/collegemanagement/repository/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.collegemanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.collegemanagement.entity.Department;
-
-public interface CourseRepo  extends JpaRepository<Department, Long>{
-
-}
